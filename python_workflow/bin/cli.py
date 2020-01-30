0bc2bc3a018d84f486d9bc3265838dce7e8125d0
donovanr <donovanr@alumni.reed.edu>
2020-01-30 23:04:22 +0000

    Deploying to example-build from master - 40ad43bfa4ea0563967cbd71553698761ff82498 🚀

--- python_workflow/bin/cli.py
+++ python_workflow/bin/cli.py
@@ -7,13 +7,9 @@ from python_workflow.bin.quilt_init import QuiltInit
 
 def cli():
     step_map = {
-        "raw": steps.Raw
+        name.lower(): step
+        for name, step in inspect.getmembers(steps)
+        if inspect.isclass(step)
     }
 
-    fire.Fire(
-        {
-            **step_map,
-            "all": All,
-            "quilt": QuiltInit,
-        }
-    )
+    fire.Fire({**step_map, "all": All, "quilt": QuiltInit})
